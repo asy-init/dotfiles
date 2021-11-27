2456ce855ab3d1d56a03afcd5a6caafe96f598a8
Asymmetric <94440862+asy-init@users.noreply.github.com>
2021-11-27 03:36:46 +0000

    Add files via upload

---
+++ fish/functions/main.fish
@@ -0,0 +1,18 @@
+function personal_functions
+    test
+end
+function ...
+    cd ../..
+end
+ 
+function ....
+     cd ../../..
+end
+
+function .....
+    cd ../../../..
+end
+function ls
+    lsd $argv
+end
+
